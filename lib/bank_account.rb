8ee37a4d6340d722c86736e2f686cf456ea54864
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-18 01:11:26 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,7 +9,21 @@ class BankAccount
     @status = "open"
   end
 
-  
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
+    balance > 0 && status == "open"
+  end
     
 
 
